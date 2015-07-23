f4016fd425aeb43392640729060f32c7259a9d24
lazybios <lazybios@gmail.com>
2015-07-24 05:24:47 +0800

    fixed bugs and add some deploy config files

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/wujing/mini-tower"
+pid "/home/wujing/mini-tower/tmp/pids/unicorn.pid"
+stderr_path "/home/wujing/mini-tower/log/unicorn_err.log"
+stdout_path "/home/wujing/mini-tower/log/unicorn_out.log"
+
+listen "/tmp/unicorn.minitower.sock"
+worker_processes 2
+timeout 30
